03e0567bc84bbba83abfed4e68b50ec5fb8da809
Smith <smithgr1@egb076000033.lancs.local>
2020-03-03 12:39:24 +0000

    adding the board

---
+++ Board.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Board
+{
+    private JFrame Hoppers!;
+    private JPanel panel;
+    private JButton button;     //WIP until I figure out what to do here
+    private GridLayout layout;
+
+
+    public Board()
+    {
+        GridLayout layout = new GridLayout(5,5)
+        panel.setLayout(layout);
+    }
+}
