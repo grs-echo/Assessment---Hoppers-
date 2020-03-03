17fc11f5692d5da632b325d2642f426821f20b0f
Smith <smithgr1@egb076000033.lancs.local>
2020-03-03 12:39:58 +0000

    adding the board and square

---
+++ Square.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Square
+{
+    private JButton water, green, red, pad;
+    private ImageIcon w, g, r, p;
+
+
+    public Square()
+    {
+        ImageIcon w = new ImageIcon("Water.png");
+        JButton water = new JButton(w);
+        //water.setIcon(i)
+        
+
+    }
+}
