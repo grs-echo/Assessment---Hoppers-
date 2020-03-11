2cebecf8da031ce999611f71be9de3e6643627ab
Smith <smithgr1@egb070000008.lancs.local>
2020-03-11 14:29:03 +0000

    tried adding code for step 3, probably barely functional, trying again tomorrow

--- Board.java
+++ Board.java
@@ -3,17 +3,79 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class Board
+public class Board //implements ActionListener
 {
-    private JFrame Hoppers!;
+    private JFrame Hoppers;
     private JPanel panel;
-    private JButton button;     //WIP until I figure out what to do here
+    private Square[] buttons;     
     private GridLayout layout;
-
+    //private boolean clicked = false;
+    //private JButton current, target;
 
     public Board()
     {
-        GridLayout layout = new GridLayout(5,5)
+        Hoppers = new JFrame("Hoppers!");
+        Hoppers.setSize(700,700);
+        panel = new JPanel();
+        Hoppers.setContentPane(panel);
+        GridLayout layout = new GridLayout(5,5);
         panel.setLayout(layout);
+        Hoppers.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        buttons = new Square[25];
+
+        for (int i=0; i<buttons.length; i++)
+        {
+            if ((i%2)==0)                       //the remainder when i is divided by 2 is 0 (even). Because i starts at 0, this will apply to odd numbered tiles instead
+            {
+                buttons[i] = new Square('p', i);
+            }
+            else                                //odd
+            {
+                buttons[i] = new Square('w', i);
+            }
+
+            if (i==6 || i==8 || i==12 || i==20 || i==24)
+            {
+                buttons[i] = new Square('g', i);
+            }
+            if (i==22)
+            {
+                buttons[i] = new Square ('r', i);
+            }
+            //(buttons[i].getButton()).addActionListener(this);
+            panel.add(buttons[i].getButton());
+        }
+
+        //3.1 - make sure it can detect when Squares are clicked - should be done, hopefully
+        //3.3 - make sure that if a user clicks a Square when already clicking on a Square, it moves the piece from A to B using moveTo - since it relies on 3.2, probably not
+        //5 - determine when the game is won and add support for multiple levels
+
+        /*
+        public void actionPerformed(ActionEvent e)
+        {
+            if (clicked == false)
+            {
+                if (e.getIcon() == "RedFrog.png")           //e? this? not sure, can't see e or this being used in any online examples
+                {
+                    e.setIcon(new ImageIcon("RedFrog2.png"));
+                }
+                else if (e.getIcon() == "GreenFrog.png")
+                {
+                    e.setIcon(newImageIcon("GreenFrog2.png"));
+                }
+                current = e;
+                clicked = true;
+                return;
+            }
+            if (clicked == true)
+            {
+                target = e;
+                moveTo(current, target);
+                clicked = false;
+            }
+        }
+        */
+
+        Hoppers.setVisible(true);
     }
 }
--- Square.java
+++ Square.java
@@ -5,16 +5,81 @@ import java.awt.event.*;
 
 public class Square
 {
-    private JButton water, green, red, pad;
-    private ImageIcon w, g, r, p;
+    private JButton button;
+    private char type;
+    private ImageIcon image;
+    private int index, x, y;
 
 
-    public Square()
+    public Square(char c, int i)
     {
-        ImageIcon w = new ImageIcon("Water.png");
-        JButton water = new JButton(w);
-        //water.setIcon(i)
+        type=c;
         
+        if (type=='w')
+            image = new ImageIcon("Water.png");
+        if (type=='g')
+            image = new ImageIcon("GreenFrog.png");
+        if (type=='r')
+            image = new ImageIcon("RedFrog.png");
+        if (type=='p')
+            image = new ImageIcon("LilyPad.png");
+        button = new JButton(image);
+        button.setIcon(image);
+        
+        index=i+1;
 
+        y = index/5;
+        y=y-1;
+        
+        x = index - y*5;
+        x=x-1;
+    }
+
+    //3.2 - write a moveTo method that can be enacted on a Square and take another Square as a parameter - maybe?
+
+    /*public void moveTo(button s, button e) //for button start and button end
+    {
+        e.setIcon(s.getImage());
+        s.setIcon(new ImageIcon("LilyPad.png"));
+
+        if (e.getIcon == "RedFrog2.png")
+        {
+            e.setIcon(new ImageIcon("RedFrog.png"));
+        }
+        if (e.getIcon == "GreenFrog2.png")
+        {
+            e.setIcon(new ImageIcon("GreenFrog.png"));
+        }
+    }*/
+
+
+    public int getIndex()
+    {
+        return index;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {   
+        return y;
+    }
+
+    public char getType()
+    {
+        return type;
+    }
+
+    /*public ImageIcon getImage()
+    {
+        return image;
+    }*/
+
+    public JButton getButton()
+    {
+        return button;
     }
 }
