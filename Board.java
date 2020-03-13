cdf577d071b30f9d9a0b1ef08ce4732719aac48f
Smith <smithgr1@egb070000020.lancs.local>
2020-03-13 15:17:32 +0000

    a few minor edits to try and fix the legality check, no success

--- Board.java
+++ Board.java
@@ -1,57 +1,57 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.*;       //import swing classes (JFrame, JPanel, JButton, JTextField, JLabel, ImageIcon, etc)
+import java.awt.*;          //import layout managers (FlowLayout, GridLayout, BorderLayout)
+import java.awt.event.*;    //need this to integrate listeners
 
 
-public class Board implements ActionListener
+public class Board implements ActionListener    //this class uses listeners, so this addition needs to be here for the class definition
 {
-    private int victoryCount;
-    private JFrame Hoppers;
-    private JPanel panel;
-    private Square[] buttons;
-    private Square working, current, target;     
-    private GridLayout layout;
-    private JOptionPane victoryMessage;
+    private int victoryCount;                   //an integer value to see if the victory condition is triggered
+    private JFrame hoppers;                     //a window in the Host OS
+    private JPanel panel;                       //a container to hold the components of the window
+    private Square[] buttons;                   //an array of Squares as established in Square.java
+    private Square working, current, target;    //Square variables as established in Square.java
+    private GridLayout layout;                  //a layout manager that fixes components into an nxm matrix
+    private JOptionPane victoryMessage;         //a dialogue window only opened if the victory condition is met
 
     public Board()
     {
-        Hoppers = new JFrame("Hoppers!");
-        Hoppers.setSize(750,750);
+        hoppers = new JFrame("Hoppers!");       
+        hoppers.setSize(750,750);               //establishes hoppers as a new JFrame called 'Hoppers!' and sets its size
         panel = new JPanel();
-        Hoppers.setContentPane(panel);
+        hoppers.setContentPane(panel);          //establishes panel as a new JPanel and sets it to the hoppers JFrame
         GridLayout layout = new GridLayout(5,5);
-        panel.setLayout(layout);
-        Hoppers.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        buttons = new Square[25];
+        panel.setLayout(layout);                //establishes layout as a new GridLayout and sets it to the panel panel
+        hoppers.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     //terminates application when the window is closed
+        buttons = new Square[25];               //creates buttons as an array of 25 squares
 
         for (int i=0; i<buttons.length; i++)
         {
-            if ((i%2)==0)                       //the remainder when i is divided by 2 is 0 (even). Because i starts at 0, this will apply to odd numbered tiles instead
+            if ((i%2)==0)                           //the remainder when i is divided by 2 is 0 (even). Because i starts at 0, this will apply to odd numbered tiles instead
             {
-                buttons[i] = new Square('p', i);
+                buttons[i] = new Square('p', i);    //odd numbered tiles are always lilypads
             }
-            else                                //odd
+            else                                    //when the index is odd
             {
-                buttons[i] = new Square('w', i);
+                buttons[i] = new Square('w', i);    //even numbered tiles are always just water
             }
 
             //level support
             if (i==6 || i==8 || i==12 || i==20 || i==24)
             {
-                buttons[i] = new Square('g', i);
+                buttons[i] = new Square('g', i);        //set the green frogs to the appropriate index values
             }
             if (i==22)
             {
-                buttons[i] = new Square ('r', i);
+                buttons[i] = new Square ('r', i);       //set the red frog to the appropriate index value
             }
-            (buttons[i].getButton()).addActionListener(this);
-            panel.add(buttons[i].getButton());
+            (buttons[i].getButton()).addActionListener(this);   //need to add an ActionListener (listener for buttons) to each Square in the array. 'this' is in reference to the instance currently being operated on
+            panel.add(buttons[i].getButton());                  //add the buttons from the array of Squares to the panel so they can be clicked on
         }
-        Hoppers.setVisible(true);
+        hoppers.setVisible(true);                               //set the window as visible
     }
 
 
-    public void actionPerformed(ActionEvent e)
+    public void actionPerformed(ActionEvent e)                          //this triggers if a button has been clicked
     {
         for (int i=0; i<buttons.length; i++)
         {
@@ -63,80 +63,81 @@ public class Board implements ActionListener
 
         if (current == null)                                            //if nothing is currently selected
         {
-            if (working.getType() == 'r')                               //if this square is a red frog
+            if (working.getType() == 'r')
             {
-                working.setImage("RedFrog2.png");                       // ...change the image of the square to the highlighted red frog
+                working.setImage("RedFrog2.png");                       //if the current square is a red frog, change the image of the square to the highlighted red frog
             }
             else if (working.getType() == 'g')
             {
-                working.setImage("GreenFrog2.png");
+                working.setImage("GreenFrog2.png");                     //if the current square is a green frog, change the image of the square to the highlighted green frog
             }
             else
             {
-                working = null;
+                working = null;                                         //if the current square is not a frog, don't pass it along to the variable 'current'
             }
-            current = working;
+            current = working;                                          //'current' takes the current square
             return;
         }
 
-        else if (working == current)                                    //the same square has been selected twice
+        else if (working == current)                    //the same square has been selected twice
         {
             if (current.getType() == 'r')
             {
-                current.setImage("RedFrog.png");
+                current.setImage("RedFrog.png");        //if that square is a red frog, return the image of the square to the regular red frog
             }
             else if (current.getType() == 'g')
             {
-                current.setImage("GreenFrog.png");
+                current.setImage("GreenFrog.png");      //if that square is a green frog, return the image of the square to the regular green frog
             }
+            working = null;
             current = null;
-            target = null;
+            target = null;                              //return all Square variables to null, act as if the click never happened
         }
 
-        else if (current != null && working.getType() == 'p')           //the second clicked square is empty
+        else if (current != null && working.getType() == 'p')                                   //the first click has happened the second clicked square is empty
         {
-            if (buttons[getMiddle(current.getIndex(), working.getIndex())].getType() == 'g')
+            if (buttons[getMiddle(current.getIndex(), working.getIndex())].getType() == 'g')    //if the square in the middle of the current square and the target square has a green frog. Because of the way this is defined, jumping over a red frog is an illegal move
             {
-                //if (current.legalityCheck(target))
+                //if (current.legalityCheck(working))
                 //{
                     target = working;
-                    current.moveTo(target);
-                    buttons[getMiddle(current.getIndex(), working.getIndex())].setImage("LilyPad.png");
-                    buttons[getMiddle(current.getIndex(), working.getIndex())].setType('p');
+                    current.moveTo(target);                                                     //perform the moveTo method on current using target as a parameter
+                    buttons[getMiddle(current.getIndex(), working.getIndex())].setToBlank();    //set the square in the middle of the two squares being operated on to a regular lilypad
+                    working = null;
                     current = null;
-                    target = null;
+                    target = null;                                                              //return all Square variables to null, act as if the click never happened
                     for (int i=0; i<buttons.length; i++)
                     {
                         if (buttons[i].getType() == 'r')
                         {
-                            victoryCount = victoryCount + 1;
+                            victoryCount = victoryCount + 1;        //every red frog increases victoryCount by 1
                         }
                         else if (buttons[i].getType() == 'g')
                         {
-                            victoryCount = victoryCount + 2;
+                            victoryCount = victoryCount + 2;        //every green frog increases victoryCount by 2
                         }
                         else
                         {
-                            victoryCount = victoryCount;
+                            victoryCount = victoryCount;            //pads and water do nothing to victoryCount
                         }
                     }
                     if (victoryCount == 1)
                     {
-                        victory();
+                        victory();                                  //if victoryCount is 1 by the end of the loop (ie the only frog is the red one), the player has won
                     }
                     else
                     {
-                        victoryCount = 0;
+                        victoryCount = 0;                           //if not, set victoryCount back to 0 so it can be recalculated on the next movement
                     }
                 //}
                 //else
                 //{
-                //    working = null;
+                //   working = null;
                 //}
             }
             else
             {
-                working = null;
+                working = null;                                     //if the square in the middle of curent and target isn't a frog, cancel the movement
             }
         }
     }
@@ -144,12 +145,11 @@ public class Board implements ActionListener
     {
         a = a + b;
         a = a / 2;
-        return a;
+        return a;                                               //an easy way to calculate the middle of two numbers
     }
-    //victory message
     //level select
     public void victory()
     {
-        JOptionPane.showMessageDialog(Hoppers, "Congratulations, you won!");
+        JOptionPane.showMessageDialog(hoppers, "Congratulations, you won!");    //a victory message displayed as a popup
     }
 }
--- Square.java
+++ Square.java
@@ -1,70 +1,71 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-
+import javax.swing.*;       //import swing classes (JFrame, JPanel, JButton, JTextField, JLabel, ImageIcon, etc)
 
 public class Square
 {
-    private JButton button;
-    private char type;
-    private ImageIcon image;
-    private int index, x, y;
+    private JButton button;         //A pushable button
+    private char type;              //the 'type' that the square has. An easy way to represent the image that the square has
+    private ImageIcon image;        //the picture that the square has
+    private int index, x, y;        //numbers to represent the square's position in board
 
 
-    public Square(char c, int i)
+    public Square(char c, int i)    //the square constructor requires the type and its index in the array from Board
     {
         type=c;
         
         if (type=='w')
-            image = new ImageIcon("Water.png");
+            image = new ImageIcon("Water.png");     //w is the type letter corresponding to water
         if (type=='g')
-            image = new ImageIcon("GreenFrog.png");
+            image = new ImageIcon("GreenFrog.png"); //g is the type letter corresponding to a green frog
         if (type=='r')
-            image = new ImageIcon("RedFrog.png");
+            image = new ImageIcon("RedFrog.png");   //r is the type letter corresponding to a red frog
         if (type=='p')
-            image = new ImageIcon("LilyPad.png");
+            image = new ImageIcon("LilyPad.png");   //p is the type letter corresponding to a lilypad
         button = new JButton(image);
-        button.setIcon(image);
+        button.setIcon(image);                      //the button takes the correct image for its type
         
         index = i;
 
-        y = index / 5;
-        y = 4 - y;
-        
-        x = index - y*5;
-        x= x - 1;
+        y = index / 5;      //y represents the y coordinates of the square with a specific index value
+        y = 4 - y;          //this isn't really necessary, but it works to bring the value of y more in line with the human understanding of coordinates (without this it would decrease as index increased)
+        y = y + 1;
+
+        x = index - y*5;    //x represents the x coordinates
+        x = x + 1;
     }
 
 
-    public void moveTo(Square dest) //for button start and button end
+    public void moveTo(Square dest)         //this is a method that acts on another Square; moving the image on the Square being acted on to the Square being provided as a parameter
     {
         if (type == 'r')
         {
             dest.setImage("RedFrog.png");
-            dest.setType('r');
+            dest.setType('r');              //if the type of the square being acted on is r, the destination would need to be a red frog 
         }
         else if (type == 'g')
         {
             dest.setImage("GreenFrog.png");
-            dest.setType('g');
+            dest.setType('g');              //if the type of the square being acted on is g, the destination would need to be a green frog
         }
-        setImage("LilyPad.png");
-        setType('p');
-        /*delete the one inbetween*/
+        setToBlank();                       //the original square being acted on needs to be made blank (ie a normal lilypad)
     }
 
     public boolean legalityCheck(Square dest)
     {
-        /*(pseudocode for now)
-        check the x coordinates - are they the same
-        check the y coordinates - are they the same
-        if they aren't the same, are both of them different by a modulus of two
-        if none of these, invalid move, never move target to working (working = null)*/
-        if (getX() == dest.getX() || getY() == dest.getY())
+        if (getX() == dest.getX())                      //if the x coordinates are the same
         {
-            return true;
+            if (Math.abs(getY() - dest.getY()) == 4)    //the only valid move is if the y values of the start and destination are a magnitude of 4 squares apart (this is what Math.abs calculates)
+                return true;
+            else
+                return false;
         }
-        else if (Math.abs(getX() - dest.getX()) == 2 && Math.abs(getY() - dest.getY()) == 2)
+        else if (getY() == dest.getY())                 //if the y coordinates are the same
+        {
+            if (Math.abs(getX() - dest.getX()) == 4)    //the only valid move is if the x values of the start and destination are a magnitude of 4 squares apart (this is what Math.abs calculates)
+                return true;
+            else
+                return false;
+        }
+        else if (Math.abs(getX() - dest.getX()) == 2 && Math.abs(getY() - dest.getY()) == 2)    //this is to represent a diagonal movement
         {
             return true;
         }
@@ -73,45 +74,50 @@ public class Square
             return false;
         }
     }
-    
 
     public int getIndex()
     {
-        return index;
+        return index;                       //an accessor for the index
     }
 
     public int getX()
     {
-        return x;
+        return x;                           //an accessor for the x value
     }
 
     public int getY()
     {   
-        return y;
+        return y;                           //an accessor for the y value
     }
 
     public char getType()
     {
-        return type;
+        return type;                        //an accessor for the type
     }
 
     public void setType(char s)
     {
-        type = s;
+        type = s;                           // a mutator for the type
     }
 
     public ImageIcon getImage()
     {
-        return image;
+        return image;                       //an accessor for the image
     }
 
     public void setImage(String s)
     {
-        button.setIcon(new ImageIcon(s));     
+        button.setIcon(new ImageIcon(s));   //a mutator for the image
     }
 
     public JButton getButton()
     {
-        return button;
+        return button;                      //an accessor for the button
+    }
+
+    public void setToBlank()
+    {
+        setImage("LilyPad.png");
+        setType('p');                       //a mutator to specifically turn the square into a lilypad square
     }
 }
